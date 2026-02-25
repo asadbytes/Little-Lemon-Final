8f526f2a163eb5de4f68648a6eb6d2404d17d98e
asadmaida <mr.asad.maida@gmail.com>
2026-02-25 10:15:32 +0500

    Initial Commit

---
+++ Little Lemon Final/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Little Lemon Final
+//
+//  Created by MacBook Pro on 25/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Little Lemon Final/Little_Lemon_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Lemon_FinalApp.swift
+//  Little Lemon Final
+//
+//  Created by MacBook Pro on 25/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
